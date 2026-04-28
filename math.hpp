1ec6839630608f7b1fe54a30ca52d5737d53ea6a
OwnderDuck <ownderduck@gmail.com>
2026-04-28 20:33:41 +0800

    Matrix4

--- math.hpp
+++ math.hpp
@@ -5,19 +5,48 @@
 
 struct Vector3 {
     float x,y,z;
-    Vector3 operator + (const Vector3& v) const {return {x+v.x,y+v.y,z+v.z};}
-    Vector3 operator - (const Vector3& v) const {return {x-v.x,y-v.y,z-v.z};}
-    Vector3 cross(const Vector3& v) const {return {
-        y*v.z-z*v.y,
-        z*v.x-x*v.z,
-        x*v.y-y*v.x};
+    Vector3 operator + (const Vector3& o) const {return {x+o.x,y+o.y,z+o.z};}
+    Vector3 operator - (const Vector3& o) const {return {x-o.x,y-o.y,z-o.z};}
+    Vector3 cross(const Vector3& o) const {return {
+        y*o.z-z*o.y,
+        z*o.x-x*o.z,
+        x*o.y-y*o.x};
     }
-    float dot(const Vector3& v) const {return v.x*v.x+v.y*v.y+v.z*v.z;}
+    float dot(const Vector3& o) const {return o.x*o.x+o.y*o.y+o.z*o.z;}
     float length() const {return sqrt(dot(*this));}
     Vector3 normalize() {
         float l=length();
         if (l>0) return {x/l,y/l,z/l};
         else return {0,0,0};
+    }
+};
+
+struct Matrix4 {
+    float m[4][4]={0};
+    float* operator[](int o) {return m[o];}
+    const float* operator[](int o) const {return m[o];}
+    static Matrix4 identity() {
+        Matrix4 _m;
+        _m[0][0]=1;_m[0][1]=0;_m[0][2]=0;_m[0][3]=0;
+        _m[1][0]=0;_m[1][1]=1;_m[1][2]=0;_m[1][3]=0;
+        _m[2][0]=0;_m[2][1]=0;_m[2][2]=1;_m[2][3]=0;
+        _m[3][0]=0;_m[3][1]=0;_m[3][2]=1;_m[3][3]=1;
+        return _m;
+    }
+    Matrix4 operator*(const Matrix4& o) {
+        Matrix4 ans;
+        for (int i=0;i<4;i++) {for (int j=0;j<4;j++) {for (int k=0;k<4;k++) {
+            ans[i][j]+=m[i][k]*o[k][j];
+        }}}
+        return ans;
+    }
+    Vector3 transform(const Vector3 o) const {
+        float x=o.x*m[0][0]+o.y*m[0][1]+o.z*m[0][2]+m[0][3];
+        float y=o.x*m[1][0]+o.y*m[1][1]+o.z*m[1][2]+m[1][3];
+        float z=o.x*m[2][0]+o.y*m[2][1]+o.z*m[2][2]+m[2][3];
+        float w=o.x*m[3][0]+o.y*m[3][1]+o.z*m[3][2]+m[3][3];
+        if (w!=0) return {x/w,y/w,z/w};
+        return {0,0,0};
     }
 };
 
